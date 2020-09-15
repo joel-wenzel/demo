9220ba58559e1627204aaf4133f6dd79f3b9c695
joel <joel.wenzel@flexagon.com>
2020-09-15 10:30:32 -0500

    added port

--- Terraform/oci_vm/backend.tf
+++ Terraform/oci_vm/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "consul" {
-    address = "teplt01.flexagon"
+    address = "teplt01.flexagon:8500"
     scheme  = "http"
     path    = "terraform/state/flex/oci/vm/demo1"
 	lock = true
